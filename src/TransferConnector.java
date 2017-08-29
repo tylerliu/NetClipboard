1aa237b8608fe15a3f72d206b50b411dd743870b
tylerliu <tylersiqi@163.com>
2017-08-29 10:04:46 -0400

    ..1

--- src/TransferConnector.java
+++ src/TransferConnector.java
@@ -1,22 +1,21 @@
 import java.io.*;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.UnknownHostException;
+import java.net.*;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Created by TylerLiu on 2017/08/28.
  */
 public class TransferConnector{
 
-    static final boolean isLoopBack = true;
+    static final boolean isLoopBack = false;
     static final int connectionPort = 31415;
     static DataInputStream inputStream;
     static DataOutputStream outputStream;
     static Socket socket;
     static ServerSocket serverSocket;
     static InetAddress localHost;
+    private static AtomicBoolean isConnOpen = new AtomicBoolean(false);
 
     static InetAddress getTarget(){
         if (isLoopBack) return InetAddress.getLoopbackAddress();
@@ -36,20 +35,56 @@ public class TransferConnector{
 
 
     static void connect(){
+        Thread serverThread = new Thread(TransferConnector::serverConn);
+        Thread clientThread = new Thread(TransferConnector::clientConn);
+        serverThread.start();
+        clientThread.start();
         try {
-            socket = new Socket(getTarget(), connectionPort);
-            inputStream = new DataInputStream(socket.getInputStream());
-            outputStream = new DataOutputStream(socket.getOutputStream());
-        }catch (IOException e) {
-            try {
-                System.out.println("client failed. trying server");
-                serverSocket = new ServerSocket(connectionPort);
-                socket = serverSocket.accept();
+            serverThread.join();
+            clientThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("connected?");
+    }
+
+    private static void clientConn(){
+        try {
+            Socket client_socket = new Socket(getTarget(), connectionPort);
+                if (!isConnOpen.compareAndSet(false, true)) {
+                    client_socket.close();
+                    return;
+                }
+
+                System.out.println("Opened client");
+                socket = client_socket;
                 inputStream = new DataInputStream(socket.getInputStream());
                 outputStream = new DataOutputStream(socket.getOutputStream());
-            } catch (IOException f) {
-                f.printStackTrace();
+        } catch (ConnectException c){
+            System.out.println(c.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void serverConn(){
+        try {
+            serverSocket = new ServerSocket(connectionPort);
+            Socket conn_socket = serverSocket.accept();
+            if (!isConnOpen.compareAndSet(false, true)) {
+                conn_socket.close();
+                serverSocket.close();
+                serverSocket = null;
+                return;
             }
+            System.out.println("Opened server");
+            socket = conn_socket;
+            inputStream = new DataInputStream(socket.getInputStream());
+            outputStream = new DataOutputStream(socket.getOutputStream());
+        } catch (BindException b) {
+            System.out.println(b.getMessage());
+        }catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
