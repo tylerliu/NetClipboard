6dc2488ea1b8e0b9a1e749245ddde8e9510d500e
tylerliu <tylersiqi@163.com>
2017-10-01 21:56:03 -0400

    update 2

---
+++ src/MultipleFormatInputStream.java
@@ -0,0 +1,118 @@
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Created by TylerLiu on 2017/10/01.
+ */
+public class MultipleFormatInputStream extends FilterInputStream {
+
+    public byte[] buf;
+    public byte type;
+    public int length;
+    public byte cont;
+
+    /**
+     * Creates a <code>FilterInputStream</code>
+     * by assigning the  argument <code>in</code>
+     * to the field <code>this.in</code> so as
+     * to remember it for later use.
+     *
+     * @param in the underlying input stream, or <code>null</code> if
+     *           this instance is to be created without an underlying stream.
+     */
+    public MultipleFormatInputStream(InputStream in) {
+        super(in);
+        buf = new byte[0x10000];
+    }
+
+    private void loadNext() throws IOException {
+        type = (byte) read();
+        cont = (byte) read();
+        length = read();
+        length += read() << 8;
+        read(buf, 0, length);
+    }
+
+
+    /**
+     * Used only when sure the next is string
+     */
+    protected String getString(){
+        byte ptype = type;
+        try {
+            loadNext();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (type != 1 && (type != 0 || ptype != 1)) return null;
+        if (cont == 0) return new String(buf, 0, length);
+        else return new String(buf, 0, buf.length) + getString();
+    }
+
+    private String tryString(){
+        if (type != 1) return null;
+        if (cont == 0) return new String(buf, 0, length);
+        else return new String(buf, 0, buf.length) + getString();
+    }
+
+    /**
+     * Used only when sure the next is file
+     */
+    protected void readFile(OutputStream out, boolean close){
+        byte ptype = type;
+        try {
+            loadNext();
+            if (type != 2 && (type != 0 || ptype != 2)) return;
+            out.write(buf, 0, cont == 1 ? buf.length : length);
+            if (cont == 1) readFile(out, close);
+            else if (close) out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void tryFile(OutputStream out, boolean close){
+        try {
+            if (type != 2) return;
+            out.write(buf, 0, cont == 1 ? buf.length : length);
+            if (cont == 1) readFile(out, close);
+            else if (close) out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     *
+     * @param out OutputStream if the object is file
+     * @param close OutputStream if the object is file
+     * @return [0] is the type, [1] is the data if applicable
+     */
+    protected Object[] readNext(OutputStream out, boolean close){
+        try {
+            loadNext();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        switch(type){
+            case 0:
+                return null;
+            case 1:
+                return new Object[]{1, tryString()};
+            case 2:
+                tryFile(out, close);
+                return new Object[]{2};
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        super.close();
+        buf = null;
+    }
+}
---
+++ src/MultipleFormatOutputStream.java
@@ -0,0 +1,148 @@
+import java.io.*;
+
+/**
+ * a class output stream that can transfer multiple format of file
+ *
+ * headers:
+ * byte 0: kind of format:
+ * 0: continue
+ * 1: String/SegmentedHTML
+ * 2: ZipFile
+ *
+ * byte 1: indicate the number it is length is exceeding 0XFFFF
+ * if yes, it will be joined with next one
+ * if exactly 0x10000, next one have to be length 0
+ *
+ * byte 2, 3: length of file
+ *
+ * Data
+ *
+ * Created by TylerLiu on 2017/10/01.
+ */
+public class MultipleFormatOutputStream extends FilterOutputStream {
+
+    /**
+     * The internal buffer where data is stored.
+     */
+    protected byte buf[];
+    /**
+     * The number of valid bytes in the buffer.
+     */
+    protected int count;
+    /**
+     * The format type of the buffer.
+     */
+    protected byte type;
+
+    /**
+     * Creates an output stream filter built on top of the specified
+     * underlying output stream.
+     *
+     * @param out the underlying output stream to be assigned to
+     *            the field <tt>this.out</tt> for later use, or
+     *            <code>null</code> if this instance is to be
+     *            created without an underlying stream.
+     */
+    public MultipleFormatOutputStream(OutputStream out) {
+        super(out);
+        buf = new byte[0x10000];
+    }
+    /** Flush the internal buffer */
+    private void flushBuffer() throws IOException {
+        out.write(new byte[]{type, (byte)(count >> 16), (byte)(count & 0XFF), (byte)((count >> 8) & 0XFF)});
+        out.write(buf, 0, count);
+        count = 0;
+        type = 0;
+    }
+
+    /**
+     * Writes the specified byte to this buffered output stream.
+     *
+     * @param      b   the byte to be written.
+     * @exception  IOException  if an I/O error occurs.
+     */
+    public synchronized void write(int b) throws IOException {
+        if (count >= buf.length) {
+            flushBuffer();
+        }
+        buf[count++] = (byte)b;
+    }
+
+
+
+    /**
+     * Writes <code>len</code> bytes from the specified byte array
+     * starting at offset <code>off</code> to this buffered output stream.
+     *
+     * <p> Ordinarily this method stores bytes from the given array into this
+     * stream's buffer, flushing the buffer to the underlying output stream as
+     * needed.  If the requested length is at least as large as this stream's
+     * buffer, however, then this method will flush the buffer and write the
+     * bytes directly to the underlying output stream.  Thus redundant
+     * <code>BufferedOutputStream</code>s will not copy data unnecessarily.
+     *
+     * @param      b     the data.
+     * @param      off   the start offset in the data.
+     * @param      len   the number of bytes to write.
+     * @exception  IOException  if an I/O error occurs.
+     */
+    public synchronized void write(byte b[], int off, int len) throws IOException {
+        if (count >= buf.length) {
+            flushBuffer();
+        }
+
+        if (len >= buf.length - count) {
+            System.arraycopy(b, off, buf, count, buf.length - count);
+            off += buf.length - count;
+            len -= buf.length - count;
+            count = buf.length;
+            flushBuffer();
+            write(b, off, len);
+        } else {
+            System.arraycopy(b, off, buf, count, len);
+            count += len;
+        }
+    }
+
+    /**
+     * Flushes this buffered output stream. This forces any buffered
+     * output bytes to be written out to the underlying output stream.
+     *
+     * @exception  IOException  if an I/O error occurs.
+     * @see        java.io.FilterOutputStream#out
+     */
+    public synchronized void flush() throws IOException {
+        if (count != 0) flushBuffer();
+        out.flush();
+    }
+
+    //format specific operations
+
+    public synchronized void writeString(String s) throws IOException {
+        assert count == 0;
+        type = 1;
+        write(s.getBytes());
+        flushBuffer();
+    }
+
+    public synchronized void writeFile(InputStream in, boolean close) throws IOException{
+        //TODO Finish this
+        assert count == 0;
+        type = 2;
+
+        //read from input stream
+
+        count = in.read(buf);
+
+        if (count == -1) count = 0;
+
+        if (count >= buf.length) {
+            flushBuffer();
+            writeFile(in, close);
+        } else {
+            flushBuffer();
+            if (close) in.close();
+        }
+    }
+}
+
