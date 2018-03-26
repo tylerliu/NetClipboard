718071466fd3017fab89f95b8aae97f8fbc44063
tylerliu <tylersiqi@163.com>
2018-03-25 22:48:17 -0400

    debug freeze

--- src/Compressor.java
+++ src/Compressor.java
@@ -9,7 +9,7 @@ import java.util.zip.ZipOutputStream;
 class Compressor {
 
     public static void copyStream(InputStream input, OutputStream output) throws IOException {
-        byte[] buffer = new byte[8192]; // Adjust if you want
+        byte[] buffer = new byte[8192];
         int bytesRead;
         while ((bytesRead = input.read(buffer)) != -1) {
             output.write(buffer, 0, bytesRead);
@@ -25,7 +25,7 @@ class Compressor {
     public static void compress(String srcPathName, String dest) {
         File file = new File(srcPathName);
         if (!file.exists())
-            throw new RuntimeException(srcPathName + "不存在！");
+            throw new RuntimeException(srcPathName + " does not exist!");
         try {
             FileOutputStream fileOutputStream = new FileOutputStream(new File(dest));
             ZipOutputStream out = new ZipOutputStream(fileOutputStream);
@@ -66,10 +66,6 @@ class Compressor {
 
     /**
      * recursively compress a file or directory
-     *
-     * @param file
-     * @param out
-     * @param basedir
      */
     private static void compress(File file, ZipOutputStream out, String basedir) {
         System.out.println("Compress：" + basedir + file.getName());
@@ -89,7 +85,6 @@ class Compressor {
             return;
         //noinspection ConstantConditions
         for (File file : dir.listFiles()) {
-            /* 递归 */
             compress(file, out, basedir + dir.getName() + "/");
         }
     }
