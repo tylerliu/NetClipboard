1d37e9c299b101d77f6e0f50e24f62de3f7534a9
tylerliu <tylerliu@users.noreply.github.com>
2018-04-14 01:38:30 -0400

    Async (#7)
    * async init
    
    * clean up file transfer
    
    * clean up
    
    * small change

--- src/net/FileTransfer.java
+++ src/net/FileTransfer.java
@@ -5,21 +5,21 @@ import files.FileReceiver;
 import files.FileSender;
 import org.apache.commons.io.FileUtils;
 
-import java.awt.datatransfer.*;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-
 import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.io.File;
+import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.file.Files;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentLinkedDeque;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -28,27 +28,23 @@ import java.util.concurrent.Executors;
  */
 public class FileTransfer {
 
-    private static File dstFolder;
-    private static List<File> files = null;
+    private static ConcurrentLinkedDeque<File> tempFolders = new ConcurrentLinkedDeque<>();
     private static boolean isCancelled;
     private static ExecutorService executor = Executors.newSingleThreadExecutor();
     private static Cancelable transferConnector;
     private static boolean isReceiveScheduled;
-    private static boolean isFilesUsed = true;
     private static boolean isFinished;
-    private static boolean isLastRetrival = false;
 
-    public synchronized static void receiveFiles(ByteBuffer spec) {
+    public synchronized static CompletableFuture<List<File>> receiveFiles(ByteBuffer spec) {
         if (isTransferring()) cancelTransfer();
-        isLastRetrival = true;
         isReceiveScheduled = true;
-        isFilesUsed = true;
         isCancelled = false;
         isFinished = false;
-        executor.submit(() -> receiveFilesWorker(spec));
+        return CompletableFuture.supplyAsync(() -> receiveFilesWorker(spec), executor);
     }
-    private static void receiveFilesWorker(ByteBuffer spec) {
-        files = null;
+
+    private static List<File> receiveFilesWorker(ByteBuffer spec) {
+        List<File> files;
         int port = Short.toUnsignedInt(spec.getShort());
         byte[] master = new byte[spec.remaining()];
         spec.get(master);
@@ -66,35 +62,32 @@ public class FileTransfer {
             transferConnector = null;
 
             if (isCancelled) {
-                FileUtils.deleteQuietly(newDstFolder);
-                return;
+                tempFolders.add(newDstFolder);
+                deleteFolder();
+                return null;
             }
 
-            isFilesUsed = false;
             isFinished = true;
 
-            if (dstFolder != null && dstFolder.exists()) {
-                FileUtils.deleteQuietly(dstFolder);
-            }
-            dstFolder = newDstFolder;
+            deleteFolder();
+            tempFolders.add(newDstFolder);
             System.out.println("File receive done");
+            return files;
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return null;
     }
 
     public synchronized static void sendFiles(List<File> sendFiles, int port, byte[] key) {
         if (isTransferring()) cancelTransfer();
-        files = sendFiles;
-        isLastRetrival = false;
         isReceiveScheduled = true;
-        isFilesUsed = true;
         isCancelled = false;
         isFinished = false;
-        executor.submit(() -> FileTransfer.sendFilesWorker(port, key));
+        executor.submit(() -> FileTransfer.sendFilesWorker(sendFiles, port, key));
     }
 
-    public static void sendFilesWorker(int port, byte[] key) {
+    public static void sendFilesWorker(List<File> files, int port, byte[] key) {
         Cipher cipher = getCipher(key, true);
         System.out.println("File sending on port: " + port);
         FileSender sender = FileSender.sendFileListObj(port);
@@ -113,21 +106,8 @@ public class FileTransfer {
         System.out.println("File receive cancelled");
     }
 
-    public synchronized static boolean isTransferFinished() {
-        return isFinished;
-    }
-
-    public synchronized static boolean isNewlyReceived() {
-        return isFinished && !isFilesUsed;
-    }
-
     public synchronized static boolean isTransferring() {
         return !isFinished && isReceiveScheduled;
-    }
-
-    public synchronized static List<File> getFiles() {
-        isFilesUsed = true;
-        return files;
     }
 
     public static Cipher getCipher(byte[] master, boolean isEncrypt) {
@@ -144,13 +124,7 @@ public class FileTransfer {
             else cipher.init(Cipher.DECRYPT_MODE, key, gcmSpec);
             cipher.updateAAD(aad);
             return cipher;
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (NoSuchPaddingException e) {
-            e.printStackTrace();
-        } catch (InvalidKeyException e) {
-            e.printStackTrace();
-        } catch (InvalidAlgorithmParameterException e) {
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | InvalidAlgorithmParameterException e) {
             e.printStackTrace();
         }
         return null;
@@ -161,45 +135,10 @@ public class FileTransfer {
         executor.shutdown();
     }
 
-    public synchronized static boolean deleteFolder(){
-        if (!isLastRetrival || dstFolder == null) return false;
-        System.out.println("Delete Folder: " + dstFolder);
-        boolean result = FileUtils.deleteQuietly(dstFolder);
-        dstFolder = null;
-        return result;
-    }
-
-    //TODO add String flavor?
-    public static class FileTransferable implements Transferable, ClipboardOwner {
-
-        private List<File> listOfFiles;
-
-        public FileTransferable(List<File> listOfFiles) {
-            this.listOfFiles = listOfFiles;
-        }
-
-        @Override
-        public DataFlavor[] getTransferDataFlavors() {
-            return new DataFlavor[]{DataFlavor.javaFileListFlavor};
-        }
-
-        @Override
-        public boolean isDataFlavorSupported(DataFlavor flavor) {
-            return DataFlavor.javaFileListFlavor.equals(flavor);
-        }
-
-        @Override
-        public Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException, IOException {
-            if (isDataFlavorSupported(flavor)) {
-                return listOfFiles;
-            } else {
-                throw new UnsupportedFlavorException(flavor);
-            }
-        }
-
-        @Override
-        public void lostOwnership(Clipboard clipboard, Transferable contents) {
-
+    public synchronized static void deleteFolder() {
+        while (!tempFolders.isEmpty()) {
+            if (tempFolders.peek() == null || !tempFolders.peek().exists() ||
+                    FileUtils.deleteQuietly(tempFolders.peek())) tempFolders.pop();
         }
     }
 }
